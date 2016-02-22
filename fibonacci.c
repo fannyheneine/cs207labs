fdf129c268300479f2f950a95259aa2873232d88
Fanny Heneine <fannyheneine@g.harvard.edu>
2016-02-21 23:44:17 -0500

    Final Submission

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int fibonacci(int n) {
+	if(n == 1)return 0;
+	if(n == 2)return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char **argv) {
+
+	printf("%d\n", fibonacci(5));
+
+	return 0;
+}
